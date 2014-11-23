eb875398aec44da4b4ef031d8164e90c73a50814
huangtao <ht.anglenx@gmail.com>
2014-11-23 03:13:08 +0000

    add task

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,36 +1,49 @@
 libraries = FileList.new("*.bz2","*.gz")
 
 options ={
-    :geos        => '',
-    :mpi         => '',
-    :proj4       => '',
-    :geos        => '',
-    :postgresql  => '',
-    :postgis     => '',
-    :protobuf    => ''
+    :geos        => 'geos-3.4.2.tar.bz2',
+    :proj4       => 'proj-4.8.0.tar.gz',
+    :geos        => 'geos-3.4.2.tar.bz2',
+    
+    :postgresql  => 'postgresql-9.3.4.tar.bz2',
+    :postgis     => 'postgis-2.1.3.tar.gz',
+    
+    :mpi         => 'openmpi-1.6.5.tar.bz2',
+    
+    :gprotobuf   => 'protobuf-2.5.0.tar.bz2',
+    :gflags      => 'gflags-2.1.1.tar.gz',
+    :gmock       => 'gmock-1.7.0.zip',
+    :gperf       => 'gperftools-2.2.tar.gz',
+    :gtest       => 'gtest-1.7.0.zip',
+    :glog        => 'glog-0.3.3.tar.gz'
+    
 }
 def extract_file name
     suffix = File.extname(name)
     base_name = File.basename(name,".tar#{suffix}")
-    # case File.extname(name)
-    # when '.bz2'
-    #     ssh "tar jxvf #{name}"
-    # when '.gz'
-    #     ssh "tar zxvf #{name}"
-    # end
-    base_name
+    #case File.extname(name)
+    #when '.bz2'
+    #     sh "tar jxvf #{name}"
+    #when '.gz'
+    #     sh "tar zxvf #{name}"
+    #when '.zip'
+    #     sh "unzip #{name}"
+    #end
+    #base_name
+    p base_name
+    p suffix
 end
 
 desc "geos library"
 task :geos do
-    p extract_file('geos-3.4.2.tar.bz2')
+    p extract_file('gv-3.4.2.tar.bz2')
     # geos = extract_file(options[:geos])
-    # ssh "cd #{geos}"
-    # ssh "./configure --prefix=/opt/#{geos}"
-    # ssh "make"
-    # ssh "make install"
-    # ssh "ln -sf /opt/#{geos} geos"
-    # ssh "cd .."
+    # sh "cd #{geos}"
+    # sh "./configure --prefix=/opt/#{geos}"
+    # sh "make"
+    # sh "make install"
+    # sh "ln -sf /opt/#{geos} geos"
+    # sh "cd .."
 end
 
 desc "proj4 library"
